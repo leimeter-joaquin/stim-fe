c578793638e0aea32ac5097cd1d7904884fbc84b
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-08-14 15:32:25 -0300

    add prettier-plugin-tailwindcss

---
+++ src/components/Testing.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+    asd
+  </div>
+</template>
+<style scoped></style>
